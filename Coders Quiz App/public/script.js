4f579af372372181975c8f31c110288416bb3677
Adam Kwoka <adam.kwoka99@gmail.com>
2023-06-17 17:48:26 +0200

    feat: add handling for correct and wrong answer, add handling for two helpers

--- Coders Quiz App/public/script.js
+++ Coders Quiz App/public/script.js
@@ -1,8 +1,28 @@
-const question = document.querySelector('.question');
+const questionElement = document.querySelector('.question');
 const answerElements = document.querySelectorAll('.answer');
+const goodAnswerElement = document.querySelector('.goodAnswers');
+const gameBoard = document.querySelector('.gameBoard');
+const subTitle = document.querySelector('h2');
+const callToAFriendButton = document.querySelector('.callToFriend');
+const fiftyFiftyButton = document.querySelector('.fiftyFifty');
+const hintElement = document.querySelector('.hint');
 
-const fillQuestionElements = ({ question, answers }) => {
-  question.innerText = question;
+const fillQuestionElements = ({ question, answers, winner, looser }) => {
+  if (winner) {
+    gameBoard.style.display = 'none';
+    subTitle.innerText = 'You win!';
+
+    return;
+  }
+
+  if (looser) {
+    gameBoard.style.display = 'none';
+    subTitle.innerText = 'You lose! Try again!';
+
+    return;
+  }
+
+  questionElement.innerText = question;
   answerElements.forEach((answerElement, index) => {
     answerElement.innerText = answers[index];
   })
@@ -16,5 +36,67 @@ const showNextQuestion = async () => {
   
   fillQuestionElements(data);
 }
+
+const handleAnswerFeedback = (data) => {
+  goodAnswerElement.innerText = data.goodAnswers;
+
+  showNextQuestion();
+}
+
+const sendAnswer = async (answerIndex) => {
+  const response = await fetch(`/answer/${answerIndex}`, {
+    method: 'POST',
+  })
+  const data = await response.json();
+
+  handleAnswerFeedback(data);
+}
+
+answerElements.forEach((answerElement) => {
+  answerElement.addEventListener('click', (event) => {
+    const answerIndex = event.target.dataset.index;
+    
+    sendAnswer(answerIndex)
+  })
+})
+
+const handleHint = ({message}) => {
+  hintElement.innerText = message;
+}
+
+const callToAFriend = async () => {
+  const response = await fetch('/help/friend', {
+    method: 'GET',
+  })
+  const data = await response.json();
+  
+  handleHint(data);
+}
+
+callToAFriendButton.addEventListener('click', callToAFriend);
+
+const handleFiftyFiftyAnswer = ({ message, answersToRemove }) => {
+  if ( typeof message === 'string' ) {
+    handleHint({ message });
+  } else {
+    answerElements.forEach((answerElement) => {
+      if (answersToRemove.indexOf(answerElement.innerText) > -1) {
+        answerElement.innerText = '';
+      }
+    })
+  }
+}
+
+
+const fiftyFifty = async () => {
+  const response = await fetch('/help/fifty', {
+    method: 'GET',
+  })
+  const data = await response.json();
+  
+  handleFiftyFiftyAnswer(data);
+}
+
+fiftyFiftyButton.addEventListener('click', fiftyFifty);
 
 showNextQuestion()
--- Coders Quiz App/routes/game.js
+++ Coders Quiz App/routes/game.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const gameRoutes = (app) => {
   let goodAnswers = 0;
+  let isGameOver = false;
   let callToAFriendUsed = false;
   let questionToTheCrowdUsed = false;
   let fiftyFiftyUsed = false;
@@ -15,7 +16,11 @@ const gameRoutes = (app) => {
       res.json({
         winner: true, 
       })
-    } else {
+    } else if (isGameOver) {
+      res.json({
+        looser: true, 
+      })
+    }else {
       const nextQuestion = questions[goodAnswers];
       const { question, answers } = nextQuestion;
 
@@ -24,6 +29,69 @@ const gameRoutes = (app) => {
       })
     }
   });
+
+  app.post('/answer/:answerIndex', (req, res) => {
+    if ( isGameOver ) {
+      res.json({
+        looser: true, 
+      })
+    }
+
+    const { answerIndex } = req.params;
+    const question = questions[goodAnswers];
+    const resObject = {};
+
+    if (question.correctAnswer === +answerIndex) {
+      goodAnswers++;
+      resObject.correct = true;
+    } else {
+      isGameOver = true;
+      resObject.correct = false;
+    }
+
+    resObject.goodAnswers = goodAnswers;
+    res.json(resObject)
+  })
+
+  app.get('/help/friend', (req, res) => {
+
+    if (callToAFriendUsed) {
+      return res.json({
+        message: 'This helper was used'
+      })
+    }
+  
+    const isAnswerKnown = Math.random() < 0.5;
+    const { answers, correctAnswer } = questions[goodAnswers];
+    
+    callToAFriendUsed = true;
+
+    res.json({
+      message: isAnswerKnown ? `I think that the correct answer is: ${answers[correctAnswer]}` : `I don't know. Sorry!`
+    })
+  })
+
+  app.get('/help/fifty', (req, res) => {
+
+    if (fiftyFiftyUsed) {
+      return res.json({
+        message: 'This helper was used'
+      })
+    }
+
+    fiftyFiftyUsed = true;
+    
+    const { answers, correctAnswer } = questions[goodAnswers];
+    const wrongAnswers = answers.filter((_ , index) => index !== correctAnswer);
+    wrongAnswers.splice(~~(Math.random() * wrongAnswers.length), 1);
+
+    res.json({
+      answersToRemove: wrongAnswers
+    })
+  })
 }
+
+
+
 
 module.exports = gameRoutes;
