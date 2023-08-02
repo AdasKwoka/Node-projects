d498ab9067b583f53939513df8b0d455ec7fab2e
Adam Kwoka <adam.kwoka99@gmail.com>
2023-08-02 06:04:57 +0200

    feat: add new filtering and displaying

--- Express App/routes/news.js
+++ Express App/routes/news.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
-/* GET home page. */
-router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+router.get('/', async (req, res) => {
+  const search = req.query.search || '';
+
+  try {
+    const foundNews = News
+    .find({ title: new RegExp(search.trim(), 'i') })
+    .sort({ created: -1 });
+
+    const data = await foundNews.exec();
+
+    res.render('news', { title: 'News', data, search });
+  } catch (err) {
+    throw new Error(err);
+  }
+  
 });
 
 module.exports = router;
